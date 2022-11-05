355c1ab6d1b272c1f2aca11b076476b493bf7a43
Ghostpengi <47687140+Ghostpengi@users.noreply.github.com>
2022-11-05 14:12:42 +1300

    Hopefully fixed xd

--- Scenes/Chicken.gd
+++ Scenes/Chicken.gd
@@ -16,7 +16,6 @@ var lowerClampXBound = 0;
 var lowerClampYBound = 0;
 var upperClampXBound = 0;
 var upperClampYBound = 0;
-var rng = RandomNumberGenerator.new()
 
 
 
@@ -32,16 +31,11 @@ func _ready():
 	inGameTimer.start(3)
 	upperClampXBound = screen_size.x;
 	upperClampYBound = screen_size.y;
-	
-	rng.randomize()
-	
-	position.x = 200
-	position.y = 200
 
 
 
 func do_this():
-	direction = rng.randi_range(1,5)
+	direction = randi() % 4
 
 
 func _process(delta):
@@ -53,16 +47,16 @@ func _process(delta):
 	
 	
 				
-	if direction == 1:
+	if direction == 0:
 		velocity.x += 1
 		
-	if direction == 2:
+	if direction == 1:
 		velocity.x -= 1
 		
-	if direction == 3:
+	if direction == 2:
 		velocity.y -= 1
 		
-	if direction == 4:
+	if direction == 3:
 		velocity.y += 1
 			
 			
@@ -88,11 +82,9 @@ func _process(delta):
 	else:
 		$AnimatedSprite.stop()
 
-#func _process(delta):
-#	pass
+
+
 func _on_Chicken_area_entered(area):
-	#position.x = get_node("Player").position.x
-	#Change the position of the chicken to be on the area it is colliding with
 	var myTempVar = area.get_name()
 	if myTempVar == "DropZone":
 		canBeHeld = false
@@ -104,10 +96,11 @@ func _on_Chicken_area_entered(area):
 	if myTempVar == "Player" && canBeHeld:
 		position.x = area.position.x + 40
 		position.y = area.position.y
-	
+		print('its happening')
+
 
 func _on_Chicken_area_exited(area):
 	var myTempVar = area.get_name()
 	if myTempVar == "Player" && canBeHeld:
 		position.x = area.position.x + 40
-		position.y = area.position.y
+		position.y = area.position.y 
